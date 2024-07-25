783d7b7868e09718c28660c2378a60335856a9ab
Diego <diegoalonsonm@gmail.com>
2024-07-25 16:28:28 -0600

    cambios bd y crud destinos

---
+++ src/main/java/com/proyectoFinal/services/DestinoService.java
@@ -0,0 +1,16 @@
+package com.proyectoFinal.services;
+
+import com.proyectoFinal.domain.Destino;
+import java.util.List;
+
+public interface DestinoService {
+
+    public List<Destino> getDestinos();
+
+    public Destino getDestino(Destino destino);
+
+    public void save(Destino destino);
+
+    public void delete(Destino destino);
+
+}
---
+++ src/main/java/com/proyectoFinal/services/servicesImpl/DestinoServiceImpl.java
@@ -0,0 +1,43 @@
+package com.proyectoFinal.services.servicesImpl;
+
+import com.proyectoFinal.dao.DestinoDao;
+import com.proyectoFinal.domain.Destino;
+import com.proyectoFinal.services.DestinoService;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class DestinoServiceImpl implements DestinoService {
+
+    @Autowired
+    private DestinoDao destinoDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Destino> getDestinos() {
+        var listaDestinos = destinoDao.findAll();
+
+        return listaDestinos;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Destino getDestino(Destino destino) {
+        return destinoDao.findById(destino.getIdDestino()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Destino destino) {
+        destinoDao.save(destino);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Destino destino) {
+        destinoDao.delete(destino);
+    }
+
+}
